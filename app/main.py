49a4fa4abdad6a6dc96da0976c7e4af7a1229506
ANAND RAJ B <anandrajb@venzotechnologies.com>
2025-01-14 14:19:26 +0000

    updated api endpoints

--- app/main.py
+++ app/main.py
@@ -1,21 +1,12 @@
-import asyncio
-from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
+from typing import Any, Dict, List, Optional, Union
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-from fastapi.templating import Jinja2Templates
-
-# from app.database import db
 
 
 app = FastAPI(
     title="Kavida.ai Task",
     version="1.0.0",
 )
-
-
-templates = Jinja2Templates(directory="templates")
 
 
 origins = [
@@ -31,6 +22,41 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def get():
-    return JSONResponse({"message": "Hello World"})
+def validate_json(
+    data: Union[Dict, List], 
+    optional_fields: Optional[List[str]] = None
+) -> Dict[str, Any]:
+    optional_fields = optional_fields if optional_fields is not None else []
+
+    def check_nulls(obj, path='', invalid_fields=None):
+        if invalid_fields is None:
+            invalid_fields = []
+        
+        if isinstance(obj, dict):
+            for key, value in obj.items():
+                field_path = f"{path}.{key}" if path else key
+                if value is None and field_path not in optional_fields:
+                    invalid_fields.append(field_path)
+                elif isinstance(value, (dict, list)):
+                    check_nulls(value, field_path, invalid_fields)
+        elif isinstance(obj, list):
+            for index, item in enumerate(obj):
+                check_nulls(item, f"{path}[{index}]", invalid_fields)
+        
+        return invalid_fields
+
+    invalid_fields = check_nulls(data)
+    
+    if invalid_fields:
+        return {"status": "error", "invalid_fields": invalid_fields}
+    return {"status": "success"}
+
+@app.post("/validate/")
+async def validate_incoming_json(request: Request):
+    try:
+        body = await request.json()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid JSON format , kindly retry with corrct format") from e
+    optional_fields = request.query_params.getlist("optional_fields")
+    result = validate_json(body, optional_fields=optional_fields)
+    return result
